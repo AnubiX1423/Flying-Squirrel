b65701b0ae168b9321ea95540bf18a1a9ff24ddf
Buxi <88042210+AnubiX1423@users.noreply.github.com>
2025-02-24 14:36:26 +0100

    Update main.py

--- Game_Files/main.py
+++ Game_Files/main.py
@@ -6,9 +6,9 @@ from score import read_score
 from score import update_text
 
 
-#Author: AnubiX
+#Author: Buxi
 
-# Játék beállításai
+# Window settings
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 700
 FPS = 60
@@ -16,7 +16,7 @@ FPS = 60
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 
-# Restart Gomb
+# Restart Button
 class Button():
         def __init__(self, x, y, image):
             self.image = image
@@ -25,11 +25,11 @@ class Button():
         
         def draw(self):
             clicked = False  
-            #eger helye
+            #mouse location
             mouse_pos = pygame.mouse.get_pos()
-             #eger a gombon van-e  
+             #mouse position check
             if self.rect.collidepoint(mouse_pos):
-                #Kattintás vizsgálat
+                #click check
                 if pygame.mouse.get_pressed()[0] == 1:
                   clicked = True  
                 
@@ -40,7 +40,7 @@ def reset():
     wood_group.empty()
     squirrel.rect.x = 130
     squirrel.rect.y = SCREEN_HEIGHT // 2
-    score = 0 #másik score, nem a fo
+    score = 0 
     return score
     
 def main():
@@ -49,27 +49,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Flying Squirrel")
 
-    global wood_group # hogy elerje a reset function
-    # Sprite csoportok
+    global wood_group 
+    # Sprite groupes
     squirrel_group = pygame.sprite.Group()
     wood_group = pygame.sprite.Group()
 
-    global squirrel # hogy elerje a reset function
-    # Mókus létrehozása
+    global squirrel 
+    # Create Squirrel
     squirrel = Squirrel(130, SCREEN_HEIGHT // 2)
     squirrel_group.add(squirrel)
 
-    # Betűtípus
+    # Texts/Fonts
     font = pygame.font.SysFont("Bradley Hand", 50)
     font2 = pygame.font.SysFont("Bradley Hand", 30)
     white = (255, 255, 255)
     
-    # Képek betöltése
+    # Load Images
     background = pygame.image.load("images/background.png")
     ground = pygame.image.load("images/ground.png")
     button_image = pygame.image.load("images/button.png")
     
-    # Változók
+    # Variables
     clock = pygame.time.Clock()
     ground_move = 0
     move_speed = 4
@@ -81,7 +81,7 @@ def main():
     wood_timer = pygame.time.get_ticks()
 
     
-    #Restart gomb
+    #Restart Button poz
     button = Button(SCREEN_WIDTH // 2 - 30, SCREEN_HEIGHT // 2 - 50, button_image)
     
     while running:
@@ -90,19 +90,19 @@ def main():
         best_score = read_score()
         best_score_title = f"Eddigi legjobb pontszám: {best_score}" 
 
-        # Mókus és fatörzs rajzolása
+        # Draw Squirrel and Woods
         squirrel_group.draw(screen)
         squirrel_group.update(start, game_over)
         wood_group.draw(screen)
 
-        # Mozgó talaj
+        # Ground
         screen.blit(ground, (ground_move, 600))
         if start and not game_over:
             ground_move -= move_speed
             if abs(ground_move) > 40:
                 ground_move = 0
 
-        # Új fatörzsek generálása
+        # Generating new Woods
         if start and not game_over:
             if pygame.time.get_ticks() - wood_timer > 1200:
                 wood_height = random.randint(-100, 100)
@@ -111,16 +111,16 @@ def main():
                 wood_timer = pygame.time.get_ticks()
             wood_group.update(move_speed)
             
-        #Mókus ütközik egy fával
+        #Hitbox check
         if pygame.sprite.spritecollide(squirrel, wood_group, False, pygame.sprite.collide_mask) or squirrel.rect.top < 0:
               game_over = True
               
-        #Mókus leesika  földre
+        #Hotbox check with ground
         if squirrel.rect.bottom > 670:
             game_over = True
             start = False
         
-        #Pontszám növelése, ha a mókus áthalad egy fatörzsön
+        # Point
         if len(wood_group) > 0:
             if squirrel.rect.centerx < wood_group.sprites()[0].rect.right and not pass_wood:
                 pass_wood = True
@@ -130,11 +130,11 @@ def main():
                 score += 1
                 pass_wood = False
                       
-        #Pontszam vizsgálat/text update
+        # Point updtae
         if score > best_score:
            update_text(score)
            
-        # Pontszám megjelenítés
+        # Draw point 
         score_text = font.render(str(score), True, white)
         screen.blit(score_text, (SCREEN_WIDTH // 2 - score_text.get_width() // 2, 30))
 
@@ -149,7 +149,7 @@ def main():
                 game_over = False
                 score = reset()
                 
-        # Kilépés
+        # Quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
