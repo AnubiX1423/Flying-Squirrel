265ced455e8d0442ea943d20188ee3c8c677a480
Buxi <88042210+AnubiX1423@users.noreply.github.com>
2025-02-24 14:32:30 +0100

    Update wood.py

--- Game_Files/wood.py
+++ Game_Files/wood.py
@@ -7,10 +7,10 @@ class Wood(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         wood_space = 175
 
-        if pos == 1:  # Felső fatörzs
+        if pos == 1:  # First Log
             self.image = pygame.transform.flip(self.image, False, True)
             self.rect.bottomleft = (x, y - wood_space // 2)
-        elif pos == -1:  # Alsó fatörzs
+        elif pos == -1:  # Lower Log
             self.rect.topleft = (x, y + wood_space // 2)
 
         self.mask = pygame.mask.from_surface(self.image)
